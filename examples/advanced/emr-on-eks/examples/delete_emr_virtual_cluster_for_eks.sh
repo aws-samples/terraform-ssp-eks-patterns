5125d8ab8c0983cdf7468f1ab5fe5d30b03d8002
Vara Bonthu <vara.bonthu@gmail.com>
2021-11-20 17:36:44 +0000

    Updated scripts for EMR on EKS blog

--- examples/advanced/emr-on-eks/examples/delete_emr_virtual_cluster_for_eks.sh
+++ examples/advanced/emr-on-eks/examples/delete_emr_virtual_cluster_for_eks.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-EKS_CLUSTER_ID='aws001-preprod-test-eks'
+EMR_VIRTUAL_CLUSTER_ID=$1 # Expects Input parameter to delete EMR Virtual cluster id
 
-export VIRTUAL_CLUSTER_ID=$(aws emr-containers list-virtual-clusters --query "virtualClusters[?name=='${EKS_CLUSTER_ID}' && state=='RUNNING'].id" --output text)
+export VIRTUAL_CLUSTER_ID=$(aws emr-containers list-virtual-clusters --query "virtualClusters[?name=='${EMR_VIRTUAL_CLUSTER_ID}' && state=='RUNNING'].id" --output text)
 
 # DELETE EMR VIRTUAL CLUSTER
 if [[ $VIRTUAL_CLUSTER_ID != "" ]]; then
-  echo "Found Cluster $EKS_CLUSTER_ID ; Deleting now..."
+  echo "Found Cluster $EMR_VIRTUAL_CLUSTER_ID ; Deleting now..."
   aws emr-containers delete-virtual-cluster --id $VIRTUAL_CLUSTER_ID
 else
-  echo "Cluster is not running with this name $EKS_CLUSTER_ID"
+  echo "Cluster is not running with this name $EMR_VIRTUAL_CLUSTER_ID"
 fi
--- examples/advanced/emr-on-eks/examples/pyspark_sample_pi/5-spark-job-with-AMP-AMG.sh
+++ examples/advanced/emr-on-eks/examples/pyspark_sample_pi/5-spark-job-with-AMP-AMG.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
-#  You can use pod template files to define the driver or executor pod’s configurations that Spark configurations do not support.
-# see Pod Template (https://spark.apache.org/docs/3.0.0-preview/running-on-kubernetes.html#pod-template).
-
 # INPUT VARIABLES 
+EMR_VIRTUAL_CLUSTER_ID=$1 # EMR Cluster ID
+S3_BUCKET=$2              # This script requires s3 bucket as input parameter e.g., s3://<bucket-name>
+
 EMR_ON_EKS_ROLE_ID="aws001-preprod-test-emr-eks-data-team-a"       # Replace EMR IAM role with your ID
-EKS_CLUSTER_ID='aws001-preprod-test-eks'                           # Replace cluster id with your id
 EMR_ON_EKS_NAMESPACE='emr-data-team-a'                             # Replace namespace with your namespace
 JOB_NAME='taxidata'                                   
-
-S3_BUCKET='s3://<enter your s3 bucket name>'                   # Create your own s3 bucket and replace this value
-CW_LOG_GROUP="/emr-on-eks-logs/${EKS_CLUSTER_ID}/${EMR_ON_EKS_NAMESPACE}" # Create CW Log group if not exist
-SPARK_JOB_S3_PATH="${S3_BUCKET}/${EKS_CLUSTER_ID}/${EMR_ON_EKS_NAMESPACE}/${JOB_NAME}"
-
-# Step1: COPY POD TEMPLATES TO S3 Bucket
+CW_LOG_GROUP="/emr-on-eks-logs/${EMR_VIRTUAL_CLUSTER_ID}/${EMR_ON_EKS_NAMESPACE}" # Create CW Log group if not exist
+SPARK_JOB_S3_PATH="${S3_BUCKET}/${EMR_VIRTUAL_CLUSTER_ID}/${EMR_ON_EKS_NAMESPACE}/${JOB_NAME}"
+EMR_EKS_RELEASE_LABEL="emr-6.3.0-latest"
+# Copy PySpark and Test data to S3 bucket 
 aws s3 sync ./pyspark/ "${SPARK_JOB_S3_PATH}/"
 
-# FIND ROLE ARN and EMR VIRTUAL CLUSTER ID 
+# Find Role ARN and EMR Virutal Cluster ID
 EMR_ROLE_ARN=$(aws iam get-role --role-name $EMR_ON_EKS_ROLE_ID --query Role.Arn --output text)
-VIRTUAL_CLUSTER_ID=$(aws emr-containers list-virtual-clusters --query "virtualClusters[?name=='${EKS_CLUSTER_ID}' && state=='RUNNING'].id" --output text)
+VIRTUAL_CLUSTER_ID=$(aws emr-containers list-virtual-clusters --query "virtualClusters[?name=='${EMR_VIRTUAL_CLUSTER_ID}' && state=='RUNNING'].id" --output text)
 
 # Execute Spark job
 if [[ $VIRTUAL_CLUSTER_ID != "" ]]; then
-  echo "Found Cluster $EKS_CLUSTER_ID; Executing the Spark job now..."
+  echo "Found Cluster $EMR_VIRTUAL_CLUSTER_ID; Executing the Spark job now..."
   aws emr-containers start-job-run \
     --virtual-cluster-id $VIRTUAL_CLUSTER_ID \
     --name $JOB_NAME \
     --execution-role-arn $EMR_ROLE_ARN \
-    --release-label emr-6.3.0-latest \
+    --release-label $EMR_EKS_RELEASE_LABEL \
     --job-driver '{
       "sparkSubmitJobDriver": {
-        "entryPoint": "'"$SPARK_JOB_S3_PATH"'/scripts/spark-taxi-trip-data.py",
+        "entryPoint": "'"$SPARK_JOB_S3_PATH"'/scripts/sample-spark-taxi-trip.py",
         "entryPointArguments": ["'"$SPARK_JOB_S3_PATH"'/input/taxi-trip-data/",
-          "'"$SPARK_JOB_S3_PATH"'/ouput/taxi-trip-data/", "taxitripdata"
+          "'"$SPARK_JOB_S3_PATH"'/ouput/taxi-trip-data/"
         ],
         "sparkSubmitParameters": "--conf spark.executor.instances=2 --conf spark.executor.memory=2G --conf spark.executor.cores=2 --conf spark.driver.cores=1 --conf spark.metrics.conf.*.sink.prometheusServlet.class=org.apache.spark.metrics.sink.PrometheusServlet --conf spark.metrics.conf.*.sink.prometheusServlet.path=/metrics/prometheus --conf master.sink.prometheusServlet.path=/metrics/master/prometheus --conf applications.sink.prometheusServlet.path=/metrics/applications/prometheus --conf spark.ui.prometheus.enabled=true --conf spark.executor.processTreeMetrics.enabled=true --conf spark.kubernetes.driver.annotation.prometheus.io/scrape=true --conf spark.kubernetes.driver.annotation.prometheus.io/path=/metrics/prometheus/ --conf spark.kubernetes.driver.annotation.prometheus.io/port=4040 --conf spark.kubernetes.driver.service.annotation.prometheus.io/scrape=true --conf spark.kubernetes.driver.service.annotation.prometheus.io/path=/metrics/executors/prometheus/ --conf spark.kubernetes.driver.service.annotation.prometheus.io/port=4040"
       }
@@ -41,7 +38,6 @@ if [[ $VIRTUAL_CLUSTER_ID != "" ]]; then
           {
             "classification": "spark-defaults", 
             "properties": {
-              "spark.hadoop.hive.metastore.client.factory.class":"com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory",
               "spark.driver.memory":"2G",
               "spark.kubernetes.executor.podNamePrefix":"taxidata-executor"
             }
@@ -56,6 +52,6 @@ if [[ $VIRTUAL_CLUSTER_ID != "" ]]; then
       }
     }'
 else
-  echo "Cluster is not in running state $EKS_CLUSTER_ID"
+  echo "Cluster is not in running state $EMR_VIRTUAL_CLUSTER_ID"
 fi
 
